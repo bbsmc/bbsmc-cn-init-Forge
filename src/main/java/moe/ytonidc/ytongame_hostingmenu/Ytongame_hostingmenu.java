352142fcdf8fb4ecd3301bba8577792fbce443d6
Kotori0629 <79943751@qq.com>
2026-01-25 02:34:12 +0800

    fix: 修复语言切换后 RegionDetector 未同步更新的问题
    - 在 RegionDetector 中添加 cachedLanguage 缓存和 refreshLanguage 方法
    - 切换语言后调用 refreshLanguage 确保 shouldShowAds 使用最新语言

--- src/main/java/moe/ytonidc/ytongame_hostingmenu/Ytongame_hostingmenu.java
+++ src/main/java/moe/ytonidc/ytongame_hostingmenu/Ytongame_hostingmenu.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.mojang.logging.LogUtils;
 import moe.ytonidc.ytongame_hostingmenu.client.HostingPackage;
+import moe.ytonidc.ytongame_hostingmenu.client.RegionDetector;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.repository.Pack;
@@ -56,6 +57,7 @@ public class Ytongame_hostingmenu {
             if (!"zh_cn".equals(currentLang)) {
                 LOGGER.info("Current language is '{}', switching to zh_cn", currentLang);
                 mc.getLanguageManager().setSelected("zh_cn");
+                RegionDetector.refreshLanguage("zh_cn");
                 mc.reloadResourcePacks();
             }
 
