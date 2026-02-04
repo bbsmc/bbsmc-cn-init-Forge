ea9d807cae1db17acf1b292b449eaaf6bb1bcf12
Kotori0629 <79943751@qq.com>
2026-02-05 01:21:09 +0800

    chore: 版本更新至 1.0.7

--- src/main/java/moe/ytonidc/ytongame_hostingmenu/Ytongame_hostingmenu.java
+++ src/main/java/moe/ytonidc/ytongame_hostingmenu/Ytongame_hostingmenu.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class Ytongame_hostingmenu {
     public static final String MODID = "ytongame_hostingmenu";
     public static final String NAME = "YtonGame-HostingMenu";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
     
     public static final ResourceLocation hostingLogo = new ResourceLocation(MODID, "textures/gui/logo_ytongame.png");
     public static final Logger LOGGER = LogManager.getLogger();
