fae8770c206a098f3fc11da2742ee4b737cd3fdf
Kotori0629 <79943751@qq.com>
2026-03-01 21:00:44 +0800

    refactor: 移除多语言化，直接使用中文文本
    删除 en_us.lang，代码中移除 I18n 调用改为直接中文字符串。
    修正"整合包"为"汉化包"，优化广告删除说明文案。

--- src/main/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
+++ src/main/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
@@ -5,7 +5,6 @@ import moe.ytonidc.ytongame_hostingmenu.Ytongame_hostingmenu;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.resources.I18n;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,18 +13,20 @@ import java.util.List;
 
 public class LocalizationNoticeScreen extends GuiScreen {
 
-    private static final String TITLE_KEY = "ytongame_hostingmenu.notice.title";
-    private static final String[] NOTICE_KEYS = {
-        "ytongame_hostingmenu.notice.intro",
+    private static final String TITLE_TEXT = "\u00a7l\u00a7eBBSMC汉化包使用须知";
+    private static final String[] NOTICE_LINES = {
+        "\u00a7f感谢您选择BBSMC汉化包，在正式使用BBSMC汉化包进行游戏前，我们希望您能阅读以下内容。",
         "",
-        "ytongame_hostingmenu.notice.section1_title",
-        "ytongame_hostingmenu.notice.section1_line1",
-        "ytongame_hostingmenu.notice.section1_line2",
+        "\u00a76\u00a7l一、汉化内容",
+        "\u00a7fBBSMC汉化包通过AI翻译+人工精校生成，汉化内容势必存在少量问题，如果您发现了任何汉化质量问题，您可以前往我们的网站：\u00a7n\u00a7bhttps://bbsmc.net\u00a7r\u00a7f，加入我们的玩家QQ群进行反馈，我们会及时处理您的反馈，并重新发布修改后的汉化包。",
+        "\u00a7f若您当前游玩的整合包已经有完整的人工翻译，我们也相当欢迎您使用更精准更优质的人工翻译。",
         "",
-        "ytongame_hostingmenu.notice.section2_title",
-        "ytongame_hostingmenu.notice.section2_line1",
+        "\u00a76\u00a7l二、广告内容",
+        "\u00a7fBBSMC汉化包含有仅出现在服务器多人列表和创建世界导航标签页面的服务器广告，不会对游戏体验造成影响。我们需要一定的收入来支撑汉化服务器的运转。\u00a7c\u00a7l如果觉得广告影响游戏体验，请先点击\"拒绝并退出\"关闭游戏，然后手动删除mods文件夹内的YTGame-HostingMenu.jar文件，再重新启动游戏即可。",
         ""
     };
+    private static final String AGREE_TEXT = "同意并继续";
+    private static final String DECLINE_TEXT = "拒绝并退出";
 
     private final JsonObject modpackJson;
     private final List<String> languagePacks;
@@ -51,19 +52,16 @@ public class LocalizationNoticeScreen extends GuiScreen {
         int startX = (this.width - totalWidth) / 2;
         int buttonY = this.height - 40;
 
-        this.buttonList.add(new GuiButton(0, startX, buttonY, buttonWidth, buttonHeight,
-                I18n.format("ytongame_hostingmenu.notice.agree")));
-        this.buttonList.add(new GuiButton(1, startX + buttonWidth + gap, buttonY, buttonWidth, buttonHeight,
-                I18n.format("ytongame_hostingmenu.notice.decline")));
+        this.buttonList.add(new GuiButton(0, startX, buttonY, buttonWidth, buttonHeight, AGREE_TEXT));
+        this.buttonList.add(new GuiButton(1, startX + buttonWidth + gap, buttonY, buttonWidth, buttonHeight, DECLINE_TEXT));
 
         // 预计算自动换行
         wrappedLines.clear();
         int maxWidth = this.width - 60;
-        for (String key : NOTICE_KEYS) {
-            if (key.isEmpty()) {
+        for (String line : NOTICE_LINES) {
+            if (line.isEmpty()) {
                 wrappedLines.add("");
             } else {
-                String line = I18n.format(key);
                 wrappedLines.addAll(this.fontRenderer.listFormattedStringToWidth(line, maxWidth));
             }
         }
@@ -103,7 +101,7 @@ public class LocalizationNoticeScreen extends GuiScreen {
         this.drawDefaultBackground();
 
         // 标题
-        this.drawCenteredString(this.fontRenderer, I18n.format(TITLE_KEY), this.width / 2, 15, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, TITLE_TEXT, this.width / 2, 15, 0xFFFFFF);
 
         // 须知文案
         int textX = 30;
