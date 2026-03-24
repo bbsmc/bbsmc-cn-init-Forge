b8713771f80198c3d8fd6b22e3b2a2d18431829b
Laotouy <18922569@qq.com>
2026-03-24 21:34:38 +0800

    修复: 移除 save() 调用避免初始化阶段与其他模组冲突

--- src/main/java/net/bbsmc/cninit/mixin/GameOptionsMixin.java
+++ src/main/java/net/bbsmc/cninit/mixin/GameOptionsMixin.java
@@ -56,7 +56,8 @@ public class GameOptionsMixin {
                 }
             }
 
-            ((GameSettings) (Object) this).save();
+            // 不调用 save()：初始化阶段其他模组可能未就绪，
+            // 字段修改在内存中立即生效，游戏正常退出时自动保存
 
         } catch (Exception e) {
             LogManager.getLogger("bbsmc-cn-init")
